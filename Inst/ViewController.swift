3cea80d062bf68ad616489713a18c68f50139af4
YutakaHorikawa <gozutakemaru@gmail.com>
2014-10-04 02:11:08 +0900

    ビデオ撮影&保存

--- Inst/ViewController.swift
+++ Inst/ViewController.swift
@@ -1,15 +1,9 @@
-//
-//  ViewController.swift
-//  Inst
-//
-//  Created by Horikawa Yutaka on 2014/09/28.
-//  Copyright (c) 2014年 Lb. All rights reserved.
-//
 
 import UIKit
 import AVFoundation
+import AssetsLibrary
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, AVCaptureFileOutputRecordingDelegate {
 
     // セッション
     var mySession : AVCaptureSession!
@@ -17,6 +11,12 @@ class ViewController: UIViewController {
     var myDevice : AVCaptureDevice!
     // 画像のアウトプット
     var myImageOutput : AVCaptureStillImageOutput!
+    // ビデオのアウトプット
+    var myVideoOutput : AVCaptureMovieFileOutput!
+    // スタートボタン
+    var myButtonStart : UIButton!
+    // ストップボタン
+    var myButtonStop : UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,24 +26,39 @@ class ViewController: UIViewController {
         // デバイス一覧の取得
         let devices = AVCaptureDevice.devices()
         
-        // バックカメラをmyDeviceに格納
+        // バックライトをmyDeviceに格納
         for device in devices{
             if(device.position == AVCaptureDevicePosition.Back){
                 myDevice = device as AVCaptureDevice
             }
         }
         
-        // バックカメラからVideoInputを取得.
+        // バックカメラを取得.
         let videoInput = AVCaptureDeviceInput.deviceInputWithDevice(myDevice, error: nil) as AVCaptureDeviceInput
         
-        // セッションに追加
+        // ビデオをセッションのInputに追加
         mySession.addInput(videoInput)
+        
+        // マイクを取得.
+        let audioCaptureDevice = AVCaptureDevice.devicesWithMediaType(AVMediaTypeAudio)
+        
+        // マイクをセッションのInputに追加
+        let audioInput = AVCaptureDeviceInput.deviceInputWithDevice(audioCaptureDevice[0] as AVCaptureDevice, error: nil)  as AVCaptureInput
+        
+        // オーディオをセッションに追加
+        mySession.addInput(audioInput);
         
         // 出力先を生成.
         myImageOutput = AVCaptureStillImageOutput()
         
         // セッションに追加.
         mySession.addOutput(myImageOutput)
+        
+        // 動画の保存
+        myVideoOutput = AVCaptureMovieFileOutput()
+        
+        // ビデオ出力をOutputに追加.
+        mySession.addOutput(myVideoOutput)
         
         // 画像を表示するレイヤーを生成.
         let myVideoLayer : AVCaptureVideoPreviewLayer = AVCaptureVideoPreviewLayer.layerWithSession(mySession) as AVCaptureVideoPreviewLayer
@@ -57,16 +72,38 @@ class ViewController: UIViewController {
         mySession.startRunning()
         
         // UIボタンを作成
-        let myButton = UIButton(frame: CGRectMake(0,0,120,50))
-        myButton.backgroundColor = UIColor.redColor();
-        myButton.layer.masksToBounds = true
-        myButton.setTitle("撮影", forState: .Normal)
-        myButton.layer.cornerRadius = 20.0
-        myButton.layer.position = CGPoint(x: self.view.bounds.width/2, y:self.view.bounds.height-50)
-        myButton.addTarget(self, action: "onClickMyButton:", forControlEvents: .TouchUpInside)
+        myButtonStart = UIButton(frame: CGRectMake(0,0,120,50))
+        myButtonStop = UIButton(frame: CGRectMake(0,0,120,50))
+        
+        // 背景色を設定
+        myButtonStart.backgroundColor = UIColor.redColor();
+        myButtonStop.backgroundColor = UIColor.grayColor();
+        
+        // 枠を丸くする
+        myButtonStart.layer.masksToBounds = true
+        myButtonStop.layer.masksToBounds = true
+        
+        // タイトルを設定
+        myButtonStart.setTitle("撮影", forState: .Normal)
+        myButtonStop.setTitle("停止", forState: .Normal)
+        
+        // コーナーの半径
+        myButtonStart.layer.cornerRadius = 20.0
+        myButtonStop.layer.cornerRadius = 20.0
+        
+        // ボタンの位置を指定
+        myButtonStart.layer.position = CGPoint(x: self.view.bounds.width/2 - 70, y:self.view.bounds.height-50)
+        myButtonStop.layer.position = CGPoint(x: self.view.bounds.width/2 + 70, y:self.view.bounds.height-50)
+        
+        
+        // イベントを追加
+        myButtonStart.addTarget(self, action: "onClickMyButton:", forControlEvents: .TouchUpInside)
+        myButtonStop.addTarget(self, action: "onClickMyButton:", forControlEvents: .TouchUpInside)
         
         // UIボタンをViewに追加
-        self.view.addSubview(myButton);
+        self.view.addSubview(myButtonStart);
+        self.view.addSubview(myButtonStop);
+        
      }
 
 
@@ -75,24 +112,36 @@ class ViewController: UIViewController {
     //
     func onClickMyButton(sender: UIButton){
         
-        // ビデオ出力に接続.
-        let myVideoConnection = myImageOutput.connectionWithMediaType(AVMediaTypeVideo)
-        
-        // 接続から画像を取得.
-        self.myImageOutput.captureStillImageAsynchronouslyFromConnection(myVideoConnection, completionHandler: { (imageDataBuffer, error) -> Void in
+        if( sender == myButtonStart ){
+            let paths = NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask, true)
             
-            // 取得したImageのDataBufferをJpegに変換.
-            let myImageData : NSData = AVCaptureStillImageOutput.jpegStillImageNSDataRepresentation(imageDataBuffer)
+            // フォルダ
+            let documentsDirectory = paths[0] as String
             
-            // JpegからUIIMageを作成.
-            let myImage : UIImage = UIImage(data: myImageData)
+            // ファイル名
+            let filePath : String? = "\(documentsDirectory)/test.mp4"
             
-            // アルバムに追加.
-            UIImageWriteToSavedPhotosAlbum(myImage, self, nil, nil)
+            // URI
+            let fileURL : NSURL = NSURL(fileURLWithPath: filePath!)
             
-        })
+            // 録画開始
+            myVideoOutput.startRecordingToOutputFileURL(fileURL, recordingDelegate: self)
+        } else if ( sender == myButtonStop ){
+            myVideoOutput.stopRecording()
+        }
         
     }
+    
+    func captureOutput(captureOutput: AVCaptureFileOutput!, didFinishRecordingToOutputFileAtURL outputFileURL: NSURL!, fromConnections connections: [AnyObject]!, error: NSError!) {
+        
+        //AssetsLibraryを生成する
+        let assetsLib = ALAssetsLibrary()
+        
+        //動画のパスから動画をフォトライブラリに保存する
+        assetsLib.writeVideoAtPathToSavedPhotosAlbum(outputFileURL, completionBlock: nil)
+    }
+    
+    
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
